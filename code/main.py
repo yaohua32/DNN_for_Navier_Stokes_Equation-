b45a01cdb1f8df1168118275ae0e334014f436c0
yaohua32 <11535015@zju.edu.cn>
2023-09-22 22:14:33 +0800

    update mian

--- code/main.py
+++ code/main.py
@@ -6,6 +6,8 @@
 #  */
 import numpy as np
 import torch
+import h5py
+import datetime
 
 class Example():
 
@@ -14,6 +16,31 @@ class Example():
         ''' 
         self.np_type = np_type
         self.torch_type = torch_type
+
+    def get_result_each(self, load_path:str, tloc_list:list):
+        '''
+        '''
+        submit = h5py.File("./submit/submit_"+datetime.datetime.now().strftime('%Y%m%d_%H%M%S') +".h5", 'w')
+        #
+        for tloc in tloc_list:
+            f = h5py.File(load_path+f't{tloc[0]}_{tloc[1]}/'+'Prediction.h5', 'r')
+            #
+            for key in f.keys():
+                data = f[key]
+                xy = np.around(np.reshape(data[:,:,0:2],(-1,2)), 4)
+                u = np.around(np.reshape(data[:,:,2:3],(-1,1)), 5)
+                v = np.around(np.reshape(data[:,:,3:4],(-1,1)), 5)
+                p = np.around(np.reshape(data[:,:,4:5],(-1,1)), 5)
+                #
+                frame_data = np.concatenate([xy, u, v, p], axis=1)
+                try:
+                    submit.create_dataset(key, data=frame_data)
+                except:
+                    pass
+            #
+            f.close()
+        print(submit.keys())
+        submit.close()
 
     def NS_Solver(self, save_path:str, load_path:str, load_type:str,
                   action:str, t0_loc:int=0, tT_loc:int=101, **args):
@@ -47,7 +74,7 @@ class Example():
         if action=='train':
             solver.train(save_path, load_path, model_type=load_type)
         else:
-            return solver.test(load_path, model_type=load_type)
+            return solver.pred(load_path, model_type=load_type)
 
     def train(self, save_path:str, load_path:str, tloc_list:list):
         '''
@@ -60,10 +87,24 @@ class Example():
                             load_type='model_best_error', action='train',
                             t0_loc=tloc[0], tT_loc=tloc[1], **{'lr':lr_list[inx]})
 
+    def pred(self, load_path, tloc_list):
+        #
+        for tloc in tloc_list:
+            print('tloc:', tloc)
+            demo.NS_Solver(save_path=None, 
+                           load_path=load_path+f't{tloc[0]}_{tloc[1]}/',
+                           load_type= 'model_best_error',
+                           action='test', t0_loc=tloc[0], tT_loc=tloc[1])
+        #
+        demo.get_result_each(load_path, tloc_list=tloc_list)
+
 if __name__=='__main__':
     from Problems.NS_unsteady import Problem
     demo = Example(np_type=np.float32, torch_type=torch.float32)
     tloc_list = [(0,101)]
     ###########################################
-    path = f"./code/savedModel/NS_unsteady/"
+    path = f"./code/savedModel/"
     demo.train(save_path=path, load_path=path, tloc_list=tloc_list)
+    ###########################################
+    save_path = f"./code/savedModel/"
+    demo.pred(save_path, tloc_list=tloc_list)
